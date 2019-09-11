c383446130f7a05660d20a71403d8588ed23d48d
Philip Van Every <pvanevery@unm.edu>
2019-09-10 21:49:34 -0600

    add space class

---
+++ src/model/board/space.cpp
@@ -0,0 +1,14 @@
+#include "space.hpp"
+
+namespace sw {
+namespace board {
+
+Space::Space(Color color, const std::shared_ptr<Piece> piece = nullptr)
+    : m_color(color), m_piece(piece) {}
+
+void Space::setPiece(const std::shared_ptr<Piece> piece){m_piece = piece};
+std::shared_ptr<Piece> Space::getPiece(){return m_piece};
+bool Space::isEmpty(return m_piece == nullptr);
+
+} // namespace board
+} // namespace sw
