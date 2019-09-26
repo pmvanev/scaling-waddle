ce34a4c1b79bc5f9341678d00ca30fcea6bc20ca
Philip Van Every <philip@r447444.srn.sandia.gov>
2019-09-25 20:46:24 -0600

    color class

---
+++ src/model/color.hpp
@@ -0,0 +1,13 @@
+#ifndef COLOR_HPP
+#include COLOR_HPP
+
+namespace sw {
+namespace model {
+  enum class Color {
+    WHITE,
+    BLACK
+  };
+}
+}
+
+#endif // COLOR_HPP
--- src/model/space.hpp
+++ src/model/space.hpp
@@ -1,18 +1,14 @@
 #ifndef SPACE_HPP
 #define SPACE_HPP
 
-#include "piece/piece.h"
+#include "piece.hpp"
+#include "color.hpp"
 
 namespace sw {
 namespace model {
 
 class Space {
 public:
-  enum class Color {
-    white,
-    black
-  }
-
   Space(Color color, const std::shared_ptr<Piece> piece = nullptr);
   void setPiece(const std::shared_ptr<Piece> piece);
   std::shared_ptr<Piece> getPiece();
